aa5bbbc502dbae441a9f077793a457b839a1ca0b
ychen18 <ylchen9999@gmail.com>
2021-05-16 15:53:06 -0400

    change Hello World to Hello FTC Team

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("MainActivity", "Hello World");
+        Log.d("MainActivity", "Hello FTC Team");
     }
 }
